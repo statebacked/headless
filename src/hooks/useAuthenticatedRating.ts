00a01c144d09edefe42f73c0f9cb569e08a75eb4
Adam Berger <adam@simplystated.dev>
2023-10-18 10:32:28 -0400

    feat: use authenticated rating

---
+++ src/hooks/useAuthenticatedRating.ts
@@ -0,0 +1,43 @@
+import { useMemo } from "react";
+import { StateBackedClient } from "@statebacked/client";
+import {
+  ratingMachineName,
+  aggregateRatingMachineName,
+} from "../features/authenticated-ratings/constants";
+import { Rating, useRating } from "./useRating";
+
+export type UseAuthenticatedRatingProps = {
+  orgId: string;
+  itemId: string;
+  userId: string;
+  identityProviderToken: string | (() => Promise<string>);
+  tokenProviderService?: string;
+};
+
+export type AuthenticatedRating = Rating;
+
+export const useAuthenticatedRating = ({
+  orgId,
+  identityProviderToken,
+  tokenProviderService,
+  userId,
+  ...props
+}: UseAuthenticatedRatingProps): AuthenticatedRating => {
+  const client = useMemo(
+    () =>
+      new StateBackedClient({
+        identityProviderToken: identityProviderToken,
+        orgId: orgId,
+        tokenProviderService: tokenProviderService ?? "headless-state-backed",
+      }),
+    [orgId, identityProviderToken, tokenProviderService],
+  );
+
+  return useRating({
+    ...props,
+    client,
+    userId,
+    ratingMachineName,
+    aggregateRatingMachineName,
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from "./hooks/useUnauthenticatedRating";
+export * from "./hooks/useAuthenticatedRating";

42fed917cdcccf5919ac77a3463c070701738ebd
Adam Berger <adam@simplystated.dev>
2023-10-18 12:09:52 -0400

    feat: add auth setup in cli

--- src/scripts/cli.ts
+++ src/scripts/cli.ts
@@ -14,26 +14,204 @@ main();
 async function main() {
   const command = argv.length === 0 ? "list" : argv[0];
 
-  if (command === "list") {
-    await list();
-    return;
-  }
+  try {
+    if (command === "list") {
+      await list();
+      return;
+    }
 
-  if (command !== "install") {
-    console.error(
-      "usage: `npx @statebacked/headless install [feature...]` or `npx @statebacked/headless list`",
-    );
+    if (command === "setup-auth") {
+      await setupAuth();
+      return;
+    }
+
+    if (command !== "install") {
+      console.error(
+        `State Backed Headless provides a suite of ready-made backend features, exposed via simple React hooks.
+  You can install any feature into your free State Backed organization and then use the corresponding hook in your frontend.
+
+  Usage: 
+
+  To install a feature:
+    npx @statebacked/headless install [feature...]
+    
+  To list available features:
+    npx @statebacked/headless list
+
+  To setup authentication to allow Headless features to verify your users' identities:
+    npx @statebacked/headless setup-auth auth0 --domain '<YOUR_AUTH0_DOMAIN>'
+
+    or
+
+    npx @statebacked/headless setup-auth cognito --user-pool-id '<YOUR_COGNITO_USER_POOL_ID>'  --region '<AWS_REGION_FOR_USER_POOL>'
+
+    or
+
+    npx @statebacked/headless setup-auth supabase --project '<SUPABASE_PROJCET_ID>' --secret '<SUPABASE_JWT_SECRET>'
+  `,
+      );
+      process.exit(1);
+      return;
+    }
+
+    // install
+
+    const features = argv.slice(1); // remove "install"
+    const orgId = await setup();
+    await install(features);
+
+    console.log("IMPORTANT: pass this orgId to your headless hooks: " + orgId);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+async function setupAuth() {
+  const authProvider = argv[1];
+  const args = argsToMap(argv.slice(2));
+
+  await setup();
+
+  const idpArgs =
+    authProvider === "auth0"
+      ? [
+          "upsert-auth0",
+          "--domain",
+          args["domain"],
+          "--mapping",
+          '{"sub.$": "$.sub"}',
+        ]
+      : authProvider === "cognito"
+      ? [
+          "upsert-cognito",
+          "--user-pool-id",
+          args["user-pool-id"],
+          "--region",
+          args["region"],
+          "--mapping",
+          '{"sub.$": "$.sub"}',
+        ]
+      : authProvider === "supabase"
+      ? [
+          "upsert-supabase",
+          "--project",
+          args["project"],
+          "--secret",
+          args["secret"],
+          "--mapping",
+          '{"sub.$": "$.sub"}',
+        ]
+      : null;
+
+  if (!idpArgs) {
+    console.error(`Invalid auth provider: ${authProvider}`);
     process.exit(1);
     return;
   }
 
-  // install
+  const idpPromise = new Promise((res, rej) => {
+    spawn("smply", ["identity-providers", ...idpArgs], {
+      shell: false,
+      stdio: "ignore",
+    }).on("exit", (code) => {
+      if (code === 0) {
+        res(null);
+      } else {
+        rej(new Error("failed to create identity provider"));
+      }
+    });
+  });
 
-  const features = argv.slice(1); // remove "install"
-  const orgId = await setup();
-  await install(features);
+  await Promise.all([idpPromise, ensureTokenProvider()]);
 
-  console.log("IMPORTANT: pass this orgId to your headless hooks: " + orgId);
+  console.log("Authorization setup complete.");
+}
+
+async function ensureTokenProvider() {
+  const tokenProviders = await new Promise<Array<any>>((res, rej) => {
+    const parts = [];
+    spawn("smply", ["token-providers", "list"], { shell: false, stdio: "pipe" })
+      .on("exit", (code) => {
+        if (code === 0) {
+          res(JSON.parse(Buffer.concat(parts).toString("utf8")));
+        } else {
+          rej(new Error("failed to retrieve token providers"));
+        }
+      })
+      .stdout.on("data", (data) => {
+        parts.push(data);
+      });
+  });
+
+  const headlessServiceName = "headless-state-backed";
+
+  const hasHeadlessProvider = tokenProviders.some(
+    (tp) => tp.name === headlessServiceName,
+  );
+  if (hasHeadlessProvider) {
+    return;
+  }
+
+  const keyOutput = await new Promise<string>((res, rej) => {
+    const parts = [];
+    spawn(
+      "smply",
+      ["keys", "create", "--name", headlessServiceName, "--use", "production"],
+      { shell: false, stdio: "pipe" },
+    )
+      .on("exit", (code) => {
+        if (code === 0) {
+          res(Buffer.concat(parts).toString("utf8"));
+        } else {
+          rej(new Error("failed to create key"));
+        }
+      })
+      .stdout.on("data", (data) => {
+        parts.push(data);
+      });
+  });
+
+  const keyId = JSON.parse(keyOutput.split("\n").slice(1).join("\n")).id;
+
+  await new Promise((res, rej) => {
+    spawn(
+      "smply",
+      [
+        "token-providers",
+        "upsert",
+        "--service",
+        headlessServiceName,
+        "--mapping",
+        '{"sub.$": "$.sub"}',
+        "--key",
+        keyId,
+      ],
+      { shell: false, stdio: "ignore" },
+    ).on("exit", (code) => {
+      if (code === 0) {
+        res(null);
+      } else {
+        rej(new Error("failed to create token provider"));
+      }
+    });
+  });
+}
+
+function argsToMap(args: Array<string>): Record<string, string> {
+  const map: Record<string, string> = {};
+
+  for (let i = 0; i < args.length; i = i + 2) {
+    const arg = args[i];
+    if (!arg.startsWith("--")) {
+      console.error("invalid argument: " + arg);
+      throw new Error("invalid argument: " + arg);
+    }
+
+    map[arg.slice(2)] = args[i + 1];
+  }
+
+  return map;
 }
 
 async function list() {
